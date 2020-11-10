56017e658149b1d4a8eb6a0f8a07b90a38cd16a0
Muhammad Asim <quickbooks2018@gmail.com>
2020-11-10 06:27:12 +0500

    AWS ECR build and push

--- vpc-ec2/aws/modules/aws-ec2/user-data/nodejs/user-data.sh
+++ vpc-ec2/aws/modules/aws-ec2/user-data/nodejs/user-data.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
-#Purpose: NodeJs Setup
-#App: ec2
-#OS AmazonLinux
-#Maintainer DevOps Muhammad Asim <quickbooks2018@gmail.com>
+# Codedeploy-Agent for ec2
+# Region: us-east-1
+# OS: AmazonLinux2
+# https://docs.aws.amazon.com/codedeploy/latest/userguide/codedeploy-agent-operations-install-linux.html
+# https://docs.aws.amazon.com/codedeploy/latest/userguide/codedeploy-agent.html
+# https://docs.aws.amazon.com/codedeploy/latest/userguide/instances-ec2-configure.html
 
-# ec2 Setup
+# script in zip ---> must be dos2unix
+# https://askubuntu.com/questions/304999/not-able-to-execute-a-sh-file-bin-bashm-bad-interpreter
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-. ~/.nvm/nvm.sh
+yum update -y
 
-nvm install node
+yum install ruby -y
 
-node -e "console.log('Running Node.js ' + process.version)"
+yum install wget -y 
+
+cd /home/ec2-user
+
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+
+chmod +x ./install
+
+./install auto
+
+./install auto -v releases/codedeploy-agent-###.rpm
+
+service codedeploy-agent status
+
+service codedeploy-agent start
+
+# Docker Installation
+
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+
+
+
+## END
+
+
+
+
+
 
 
 
